4f508bc88e9850c551c94877487e14d3bab62afd
Blujaytech <blujaytechnlogies@gmail.com>
2026-06-25 16:34:50 +0530

    project infra code pushed

--- main.tf
+++ main.tf
@@ -52,3 +52,53 @@ resource "aws_route_table_association" "public" {
 
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "ec2_sg" {
+
+  name = "ec2-security-group"
+
+  description = "Allow SSH and HTTP"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+
+    description = "SSH"
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    description = "HTTP"
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
